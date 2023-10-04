e7d740ab4d1c5537f043935cb5122a772cba3107
unknown <franciscorg834@gmail.com>
2023-10-04 17:47:26 -0400

    .

--- index.js
+++ index.js
@@ -66,6 +66,32 @@ app.use("/checkout", checkoutRoutes);
 app.use("/media", MediaRoutes);
 app.use("/sales", SalesRoutes)
 
+app.get('/test', (req,res) => {
+  res.sendFile(__dirname+'/index2.html')
+})
+
+const multer = require("multer");
+
+const storage = multer.diskStorage({
+    destination: function (req, file, cb) {
+      cb(null, 'uploads/')
+    },
+    filename: function (req, file, cb) {
+      const uniqueSuffix = Date.now() + '-' + Math.round(Math.random() * 1E9)
+      let extension = file.mimetype.split('/')[1]
+      cb(null, file.fieldname + '-' + uniqueSuffix+'.'+extension)
+    }
+  })
+
+function uploadFiles(req, res) {
+    console.log(req.body);
+    console.log(req.files);
+    res.json({ message: "Successfully uploaded files" });
+}
+
+const upload = multer({ storage: storage })
+
+app.post("/upload_files", upload.array("files"), uploadFiles);
 
 app.use("*", (req, res, next) => {
   console.log("* route", req.originalUrl);
